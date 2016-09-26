005f84c19087510f7dd5f6f5c79e217cc6d180da
alteste <jb.campbell13@hotmail.com>
2016-09-27 11:16:23 +1300

    Add files via upload

---
+++ Collision_v2/CollisionScript.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	private bool active = false;
+
+	public GameObject ball;
+	public GameObject p1;
+	public GameObject p2;
+
+	public Text p1ScoreCount;
+	public Text p2ScoreCount;
+
+	private int p1Score;
+	private int p2Score;
+
+	// Use this for initialization
+	void Start () {
+
+		p1Score = 0;
+		p2Score = 0;
+
+		//p1ScoreCount.text = p1Score.ToString ();
+		//p2ScoreCount.text = p2Score.ToString ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+//		if ((active == true) && (p1.gameObject.transform.position.x >= 7.6) && (p1.gameObject.transform.position.z <= 2.9) && (p1.gameObject.transform.position.z >= -2.9)) {
+//			p1Score++;	
+//			p1ScoreCount.text = p1Score.ToString ();
+//		}
+
+//		else if ((active == true) && (p2.gameObject.transform.position.x <= -7.6) && (p2.gameObject.transform.position.z <= 2.9) && (p2.gameObject.transform.position.z >= -2.9)) {
+//			p2Score++;	
+//			p2ScoreCount.text = p2Score.ToString ();
+//		}
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Ball") {
+			active = true;
+
+			p1.GetComponent<MoveScript> ().getActive (active);
+
+			ball.transform.parent = p1.transform; 
+
+			ball.transform.localPosition = new Vector3(0, 1, 0);
+
+				
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+		}
+	}
+
+	void OnCollisionExit(Collision other){
+
+		if (other.gameObject.tag == "Ball") {
+
+			ball.transform.parent = null;
+
+		}
+	}
+
+//	//void OnCollisionEnter(Collision other) {
+//		//if ((active == true) && (other.gameObject.tag == "Player")) {
+//			ball.gameObject.SetActive (true);	
+//			active = false;
+//			if (gameObject.name == "Player1") {
+//				gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+//			} else {
+//				gameObject.GetComponent<Renderer> ().material.color = Color.red;
+//			}
+//		}
+//	}
+}
---
+++ MoveScript_v2/MoveScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	public float speed;
+
+	public KeyCode p1Throw = KeyCode.Space;
+	public GameObject ball;
+
+	private Rigidbody rb;
+	private bool armed;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	public void getActive(bool active){
+		armed = active;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (gameObject.name == "Player1") {
+			transform.Translate (Input.GetAxis ("p1H") * speed * Time.deltaTime, 0, Input.GetAxis ("p1V") * speed * Time.deltaTime);
+
+			if (armed) {
+				if (Input.GetKeyDown (p1Throw)) {
+
+
+
+					ball.transform.Translate (Input.GetAxis ("p1H") * 300 * Time.deltaTime, 0, Input.GetAxis ("p1V") * speed * Time.deltaTime);
+					
+				}
+
+			} 
+
+		}
+		//else {
+			//transform.Translate (Input.GetAxis ("p2H") * speed * Time.deltaTime, 0, Input.GetAxis ("p2V") * speed * Time.deltaTime);
+		//}
+
+	}
+}
